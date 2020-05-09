e1f14c5348dad69e13ec168e7078951c8ca7d8ec
Carlos Delgado <cdelgadoguiberteau@gmail.com>
2020-05-09 20:34:08 +0200

    add basic

---
+++ app.py
@@ -0,0 +1,20 @@
+  
+from flask import Flask, redirect, url_for, render_template, flash, request
+
+import json
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+
+
+
+
+if __name__ == '__main__':
+    
+    app.run(debug=True)
