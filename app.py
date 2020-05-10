dd66893b3802e33267a39fceb246162a7d153b78
Carlos Delgado <cdelgadoguiberteau@gmail.com>
2020-05-10 22:17:15 +0200

    add front

--- app.py
+++ app.py
@@ -17,12 +17,23 @@ neouser="neo4j"
 app = Flask(__name__)
 graph=None
 
+@app.route('/<searchType>/actor/<id>')
+def actor_details(searchType, id):
+    actor = {}
+    movies={}
+
+    return render_template('ActorDetail.html',actor= actor, peliculas=movies,len = 0, searchType=searchType)
 
 @app.route('/<searchType>/details/<id>')
 def movie_details(searchType, id):
-    movie= []
+    movie= {}
+    movie["original_title"]="Test movie Title"
+    movie["poster_path"]= "https://image.tmdb.org/t/p/w220_and_h330_face/qmOtnpPihV3Wj6ukpiNIgzo4Ax7.jpg"
+    movie["release_date"]="20/20/20"
+    movie["vote_average"]= 8.6
     actors=[]
-    return render_template('MovieDetail.html',pelicula= movie, actores=actors,len = 0, searchType="searchType")
+    directors=[]
+    return render_template('MovieDetail.html',pelicula= movie, actores=actors,len = 0, director=directors, searchType=searchType)
 
 def get_10_movies():
         matcher=NodeMatcher(graph)
@@ -101,7 +112,7 @@ def api_search_movie(movie_title):
                      
         for d in p["director"]:
             d=person_info(d["id"])
-            director=Node("person", name=d["name"],birthday=d["birthday"], deathdate=d["deathday"], id=d["id"])
+            director=Node("person", name=d["name"],birthday=d["birthday"], deathdate=d["deathday"], id=d["id"], profile_path= "https://image.tmdb.org/t/p/w220_and_h330_face" + d["profile_path"])
             graph.merge(director, "person", "id")             
             directs=Relationship.type("directs")
             graph.create(directs(director, movie))
@@ -109,7 +120,7 @@ def api_search_movie(movie_title):
                 
         for d in p["cast"]:
             d=person_info(d["id"])
-            actor=Node("person", name=d["name"],birthday=d["birthday"], deathdate=d["deathday"], id=d["id"])
+            actor=Node("person", name=d["name"],birthday=d["birthday"], deathdate=d["deathday"], id=d["id"], profile_path= "https://image.tmdb.org/t/p/w220_and_h330_face" + d["profile_path"])
             graph.merge(actor, "person", "id")             
             acts_in=Relationship.type("acts_in")
             graph.create(acts_in(actor, movie))             
