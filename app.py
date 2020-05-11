d00cd77a507af26ee837729e98ce953210746dfd
Carlos Delgado <cdelgadoguiberteau@gmail.com>
2020-05-11 03:36:49 +0200

    add .

--- app.py
+++ app.py
@@ -19,18 +19,60 @@ graph=None
 
 @app.route('/<searchType>/director/<id>')
 def director_details(searchType, id):
-    director=person_info(id)
-    movies=api_search_by_director(id)    
 
-    return render_template('ActorDetail.html',actor= director, peliculas=movies,len=len(movies), searchType=searchType) 
+    movies=[]
+    matcher=NodeMatcher(graph)
+    dir=matcher.match("person").where("_.id="+id).first()
+
+    if (not dir["load"]):
+        movies=api_search_by_director(id) 
+
+        for p in movies: 
+            matcher=NodeMatcher(graph)
+            movie=matcher.match("movie").where("_.id="+str(p["id"])).first()
+
+            if (movie==None):
+                mov = tmdb.Movies(p["id"])
+                response = mov.info()
+                movie=Node("movie", original_title=response["original_title"],id=p["id"], release_date=response["release_date"],poster_path=p["poster_path"], vote_count=p["vote_count"], vote_average=p["vote_average"], load=False)
+                graph.create(movie)
+
+            directs=Relationship.type("directs")
+            graph.create(directs(dir, movie))
+
+        dir["load"]=True
+        graph.push(dir)
+
+    return render_template('ActorDetail.html',actor= dir, peliculas=movies,len=len(movies), searchType=searchType) 
 
 @app.route('/<searchType>/actor/<id>')
 def actor_details(searchType, id):
     movies=[]
     matcher=NodeMatcher(graph)
     actor=matcher.match("person").where("_.id="+id).first()
-    for a in  graph.run("MATCH (p:person)-[a:acts_in]->(m:movie) WHERE p.id={x} RETURN m", x=actor["id"]).data():
-        movies.append(a["m"])
+
+    if (not actor["load"]):
+        movies = api_search_by_actor(id)
+
+        for p in movies: 
+            matcher=NodeMatcher(graph)
+            movie=matcher.match("movie").where("_.id="+str(p["id"])).first()
+
+            if (movie==None):
+                mov = tmdb.Movies(p["id"])
+                response = mov.info()
+                movie=Node("movie", original_title=response["original_title"],id=p["id"], release_date=response["release_date"],poster_path=p["poster_path"], vote_count=p["vote_count"], vote_average=p["vote_average"], load=False)
+                graph.create(movie)
+
+            acts_in=Relationship.type("acts_in")
+            graph.create(acts_in(actor, movie))
+
+        actor["load"]=True
+        graph.push(actor)
+
+    else:
+        for a in  graph.run("MATCH (p:person)-[a:acts_in]->(m:movie) WHERE p.id={x} RETURN m", x=actor["id"]).data():
+            movies.append(a["m"])
     return render_template('ActorDetail.html',actor= actor, peliculas=movies,len =len(movies), searchType=searchType)
 
 @app.route('/<searchType>/details/<id>')
@@ -42,16 +84,21 @@ def movie_details(searchType, id):
         print("no db")
         credits = api_search_cast(movie["id"])
         cast = credits["cast"]
-        movie["load"]=True;
+        movie["load"]=True
         graph.push(movie)
         for d in cast:
-            d=person_info(d["id"])
-            if (d["profile_path"] == "None" or d["profile_path"] is None):
-                d["profile_path"]= "https://www.theprintworks.com/wp-content/themes/psBella/assets/img/film-poster-placeholder.png"
-            else:
-                d["profile_path"]= "https://image.tmdb.org/t/p/w220_and_h330_face" + d["profile_path"]              
-            actor=Node("person", name=d["name"],birthday=d["birthday"], deathdate=d["deathday"], id=d["id"], profile_path=d["profile_path"])
-            graph.merge(actor, "person", "id")
+
+            matcher=NodeMatcher(graph)
+            actor=matcher.match("person").where("_.id="+str(d["id"])).first()
+            if (actor==None):
+
+                d=person_info(d["id"])
+                if (d["profile_path"] == "None" or d["profile_path"] is None):
+                    d["profile_path"]= "https://www.theprintworks.com/wp-content/themes/psBella/assets/img/film-poster-placeholder.png"
+
+                actor=Node("person", name=d["name"],birthday=d["birthday"], deathdate=d["deathday"], id=d["id"], profile_path=d["profile_path"], load=False)
+                graph.create(actor)
+
             actors.append(actor)
             acts_in=Relationship.type("acts_in")
             graph.create(acts_in(actor, movie))
@@ -61,7 +108,7 @@ def movie_details(searchType, id):
             actors.append(a["p"])
             
     print(movie["load"])
-    return render_template('MovieDetail.html',pelicula= movie, actores=actors,len =len(actors), searchType="searchType")
+    return render_template('MovieDetail.html',pelicula= movie, actores=actors,len =len(actors), searchType=searchType)
 
 
 def get_10_movies():
@@ -85,39 +132,6 @@ def by_title_index():
         result= get_10_movies()
         return render_template('SearchPage.html',peliculas=result,len = len(result), searchType="title")
 
-
-@app.route('/director', methods=['POST', 'GET'])
-def by_director_index():
-    if request.method == "POST":
-        return redirect('/director/' + request.form.get("user_input"))
-    else:
-        result= get_10_movies()
-        return render_template('SearchPage.html',peliculas=result,len = len(result), searchType="director")
-
-
-@app.route('/genre', methods=['POST', 'GET'])
-def by_genre_index():
-    if request.method == "POST":
-        return redirect('/genre/' + request.form.get("user_input"))
-    else:
-        result= get_10_movies()
-        return render_template('SearchPage.html',peliculas=result,len = len(result), searchType="genre")
-
-@app.route('/director/<director_name')
-def search_director(director_name):
-    print(director_name)
-    nodes = graph.run("MATCH (a:person) WHERE toLower(a.name) CONTAINS toLower({x}) RETURN a", x=director_name).data()
-    result=[]
-    for x in nodes:
-        result.append(x["a"])
-    
-    #if not in the local bd, search in the api
-    if (len(result)<1):
-        result = api_search_people(director_name)["results"]
-
-    return render_template('SearchPeoplePage.html', people=result, len = len(result), searchType="director")
-
-
 @app.route('/title/<movie_title>')
 def search_movie(movie_title):
     #search in the local database
@@ -133,6 +147,43 @@ def search_movie(movie_title):
 
     return render_template('SearchPage.html', peliculas=result, len = len(result), searchType="title")
 
+
+
+@app.route('/director', methods=['POST', 'GET'])
+def by_director_index():
+    if request.method == "POST":
+        return redirect('/director/' + request.form.get("user_input"))
+    else:
+        result= get_10_movies()
+        return render_template('SearchPage.html',peliculas=result,len = len(result), searchType="director")
+
+@app.route('/director/<director_name>')
+def search_director(director_name):
+    print(director_name)
+    nodes = graph.run("MATCH (a:person) WHERE toLower(a.name) CONTAINS toLower({x}) RETURN a", x=director_name).data()
+    result=[]
+    for x in nodes:
+        result.append(x["a"])
+    
+    #if not in the local bd, search in the api
+    if (len(result)<1):
+        result = api_search_people(director_name)["results"]
+        for d in result:
+            print("DIRS")
+            print(d)
+            matcher=NodeMatcher(graph)
+            dir=matcher.match("person").where("_.id="+str(d["id"])).first()
+            if (dir==None):
+                pinfo= person_info(d["id"])
+                dir=Node("person", name=pinfo["name"],birthday=pinfo["birthday"], deathdate=pinfo["deathday"], id=pinfo["id"], profile_path=pinfo["profile_path"], load=False)
+                graph.create(dir)
+
+            result.append(dir)
+
+    return render_template('SearchPeoplePage.html', people=result, len = len(result), searchType="director")
+
+
+
 def api_search_people(p_name):
     search = tmdb.Search()
     response = search.person(query=p_name)
@@ -146,11 +197,23 @@ def api_search_people(p_name):
     return response
 
 
-
+def api_search_by_actor(actor_id):
+    people = tmdb.People(actor_id)
+    response = people.movie_credits()
+    result=[]
+    if ("cast" in response):
+        for d in response["cast"]:
+            print(d)
+            if (d["poster_path"] == "None" or d["poster_path"] is None):
+                d["poster_path"]= "https://www.theprintworks.com/wp-content/themes/psBella/assets/img/film-poster-placeholder.png"
+            else:
+                d["poster_path"]= "https://image.tmdb.org/t/p/w220_and_h330_face" + d["poster_path"]  
+            result.append(d)
+    return result
 
 def api_search_by_director(director_id):
     people = tmdb.People(director_id)
-    response = people.combined_credits()
+    response = people.movie_credits()
 
     result=[]
 
@@ -209,16 +272,14 @@ def person_info(id):
         response["profile_path"]= "https://image.tmdb.org/t/p/w220_and_h330_face" + response["profile_path"]  
     return response
 
-    
-
 def get_director_from_crew(credits):
     return [c for c in credits if c["job"] == "Director"]
-
 
 def api_search_cast(id):
     movie = tmdb.Movies(id)
     response = movie.credits()
     return response
+    
 def genres(genre_id):
     genre=tmdb.Genres()
     respones=genre.movie_list()
